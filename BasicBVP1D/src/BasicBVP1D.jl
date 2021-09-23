6de95af64b4112a571e0919493888980d5b114ff
Gideon Simpson <gideon.simpson@gmail.com>
2021-09-23 15:51:38 -0400

    added basic problem structure

--- BasicBVP1D/src/BasicBVP1D.jl
+++ BasicBVP1D/src/BasicBVP1D.jl
@@ -1,5 +1,29 @@
 module BasicBVP1D
 
-greet() = print("Hello World!")
+struct FiniteDifferenceBVPProblem
+    a # (a,b) define the domain
+    b
+    Δx # mesh spacing
+    n # number of points in the interior, excluding a and b
+    x # interior mesh
+    f # value of f at the interior nodes
+    A # the matrix
+    rhs # right hand side of our system
+end
 
+"""
+`FiniteDifferenceBVPProblem` - Initalize the data structure
+"""
+function FiniteDifferenceBVPProblem(a, b, n, f)
+    Δx = (b-a)/(n+1);
+    x = LinRange(a, b, n+2)[2:end-1];
+
+    # allocate the matrix
+    A = zeros(n, n);
+    rhs = zeros(n);
+    
+    return FiniteDifferenceBVPProblem(a, b, Δx, n, x, f, A, rhs)
+end
+
+export FiniteDifferenceBVPProblem
 end # module
