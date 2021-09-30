99a76c2ef4e42a37b5f973725663ad83734f3142
Gideon Simpson <gideon.simpson@gmail.com>
2021-09-30 16:36:37 -0400

    generalized dirichlet working

--- BasicBVP1D/src/BasicBVP1D.jl
+++ BasicBVP1D/src/BasicBVP1D.jl
@@ -1,7 +1,7 @@
 module BasicBVP1D
 
 using SparseArrays
-struct FiniteDifferenceBVPProblem{TF}
+struct FiniteDifferenceBVPProblem{TF, TBCL, TBCR}
     a # (a,b) define the domain
     b
     Δx # mesh spacing
@@ -10,7 +10,18 @@ struct FiniteDifferenceBVPProblem{TF}
     f::TF # value of f at the interior nodes
     A # the matrix
     rhs # right hand side of our system
+    left_bc::TBCL
+    right_bc::TBCR
 end
+
+
+struct DirichletBC
+    u_bc
+end
+struct NeumannBC
+    v_bc
+end
+
 
 """
 `FiniteDifferenceBVPProblem` - Initalize the data structure
@@ -23,7 +34,20 @@ function FiniteDifferenceBVPProblem(a, b, n, f)
     A = zeros(n, n);
     rhs = zeros(n);
     
-    return FiniteDifferenceBVPProblem(a, b, Δx, n, x, f, A, rhs)
+    return FiniteDifferenceBVPProblem(a, b, Δx, n, x, f, A, rhs, 
+        DirichletBC(0), DirichletBC(0))
+end
+
+function FiniteDifferenceBVPProblem(a, b, n, f, bcl::TBCL, bcr::TBCR) where {TBCL<:DirichletBC, TBCR<:DirichletBC}
+    Δx = (b-a)/(n+1);
+    x = LinRange(a, b, n+2)[2:end-1];
+
+    # allocate the matrix
+    A = zeros(n, n);
+    rhs = zeros(n);
+    
+    return FiniteDifferenceBVPProblem(a, b, Δx, n, x, f, A, rhs, 
+        bcl, bcr)
 end
 
 
@@ -38,14 +62,30 @@ function SparseFiniteDifferenceBVPProblem(a, b, n, f)
     A = spzeros(n, n);
     rhs = zeros(n);
     
-    return FiniteDifferenceBVPProblem(a, b, Δx, n, x, f, A, rhs)
+    return FiniteDifferenceBVPProblem(a, b, Δx, n, x, f, A, rhs,
+        DirichletBC(0), DirichletBC(0))
+end
+
+"""
+`SparseFiniteDifferenceBVPProblem` - Initalize the data structure
+"""
+function SparseFiniteDifferenceBVPProblem(a, b, n, f, bcl::TBCL, bcr::TBCR) where {TBCL<:DirichletBC, TBCR<:DirichletBC}
+    Δx = (b-a)/(n+1);
+    x = LinRange(a, b, n+2)[2:end-1];
+
+    # allocate the matrix
+    A = spzeros(n, n);
+    rhs = zeros(n);
+    
+    return FiniteDifferenceBVPProblem(a, b, Δx, n, x, f, A, rhs,
+        bcl, bcr)
 end
 
 
 """
 `assemble_system!` - assemble the linear system for solving
 """
-function assemble_system!(problem::FiniteDifferenceBVPProblem{TF}) where {TF<:AbstractArray}
+function assemble_system!(problem::FiniteDifferenceBVPProblem{TF, TBCL, TBCR}) where {TF<:AbstractArray, TBCL<:DirichletBC, TBCR<:DirichletBC}
 
     # build first row
     problem.A[1,1] = 2/problem.Δx^2;
@@ -62,11 +102,13 @@ function assemble_system!(problem::FiniteDifferenceBVPProblem{TF}) where {TF<:Ab
 
     # build rhs
     @. problem.rhs = problem.f;
+    problem.rhs[1] += problem.left_bc.u_bc/problem.Δx^2
+    problem.rhs[problem.n] += problem.right_bc.u_bc/problem.Δx^2
 
     problem
 end
 
-function assemble_system!(problem::FiniteDifferenceBVPProblem{TF}) where {TF<:Function}
+function assemble_system!(problem::FiniteDifferenceBVPProblem{TF, TBCL, TBCR}) where {TF<:Function, TBCL<:DirichletBC, TBCR<:DirichletBC}
 
     # build first row
     problem.A[1,1] = 2/problem.Δx^2;
@@ -83,6 +125,8 @@ function assemble_system!(problem::FiniteDifferenceBVPProblem{TF}) where {TF<:Fu
 
     # build rhs
     @. problem.rhs = problem.f(problem.x);
+    problem.rhs[1] += problem.left_bc.u_bc/problem.Δx^2
+    problem.rhs[problem.n] += problem.right_bc.u_bc/problem.Δx^2
 
     problem
 end
@@ -93,5 +137,5 @@ function solve_bvp(problem)
     return u
 end
 
-export FiniteDifferenceBVPProblem, assemble_system!, solve_bvp, SparseFiniteDifferenceBVPProblem
+export FiniteDifferenceBVPProblem, assemble_system!, solve_bvp, SparseFiniteDifferenceBVPProblem, DirichletBC, NeumannBC
 end # module
