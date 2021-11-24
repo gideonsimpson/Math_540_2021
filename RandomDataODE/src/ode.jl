6b84b88ca1c587650a82f5bc9351f27a518e104d
Gideon Simpson <gideon.simpson@gmail.com>
2021-11-23 20:19:54 -0500

    codes run in lecture

--- RandomDataODE/src/ode.jl
+++ RandomDataODE/src/ode.jl
@@ -3,11 +3,14 @@
 function f!(du, u, p, t)
     du[1] = u[1] * (1.0-u[2]);
     du[2] = u[2] * (u[1]-1.0);
+    du
 end
 
 # set up ODE
 function solve_ode(u0, Tmax)
+    # define the ODE problem
     prob = ODEProblem(f!, u0, (0., Tmax))
+    # solve the problem
     sol = solve(prob)
 
     return sol
